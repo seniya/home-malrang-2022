c9449cfdff4c23c6968599f8577342a9ecc05580
malrang <seniya2@gmail.com>
2022-07-03 22:26:21 +0900

    feat: gatsby init

---
+++ homepage-gatsby/gatsby-meta-config.js
@@ -0,0 +1,88 @@
+module.exports = {
+  title: `malrang.com`,
+  description: `Malrang Blog`,
+  language: `ko`, // `ko`, `en` => currently support versions for Korean and English
+  siteUrl: `https://www.malrang.com`,
+  ogImage: `/og-image.png`, // Path to your in the 'static' folder
+  comments: {
+    utterances: {
+      repo: `seniya/blog-comment`, // `zoomkoding/zoomkoding-gatsby-blog`,
+    },
+  },
+  ga: '0', // Google Analytics Tracking ID
+  author: {
+    name: `김우람`,
+    bio: {
+      role: `개발자`,
+      description: ['hello world', 'hellow my blog'],
+      thumbnail: 'sample.png', // Path to the image in the 'asset' folder
+    },
+    social: {
+      github: `https://github.com/seniya`,
+      linkedIn: ``, // `https://www.linkedin.com/in/jinhyeok-jeong-800871192`,
+      email: `seniya2@gmail.com`, // `zoomkoding@gmail.com`,
+    },
+  },
+
+  // metadata for About Page
+  about: {
+    timestamps: [
+      // =====       [Timestamp Sample and Structure]      =====
+      // ===== 🚫 Don't erase this sample (여기 지우지 마세요!) =====
+      {
+        date: '',
+        activity: '',
+        links: {
+          github: '',
+          post: '',
+          googlePlay: '',
+          appStore: '',
+          demo: '',
+        },
+      },
+      // ========================================================
+      // ========================================================
+      {
+        date: '2022.07 ~',
+        activity: '개인 블로그 개발 및 운영',
+        links: {
+          post: '',
+          github: '',
+          demo: '',
+        },
+      },
+    ],
+
+    projects: [
+      // =====        [Project Sample and Structure]        =====
+      // ===== 🚫 Don't erase this sample (여기 지우지 마세요!)  =====
+      {
+        title: '',
+        description: '',
+        techStack: ['', ''],
+        thumbnailUrl: '',
+        links: {
+          post: '',
+          github: '',
+          googlePlay: '',
+          appStore: '',
+          demo: '',
+        },
+      },
+      // ========================================================
+      // ========================================================
+      {
+        title: 'Simple-Finder with electron',
+        description:
+          'This application is a basic function of file finder.',
+        techStack: ['electron', 'vue'],
+        thumbnailUrl: 'finder_sample-pic-01.png',
+        links: {
+          post: '/gatsby-starter-zoomkoding-introduction',
+          github: 'https://github.com/seniya/electron-finder-normal',
+          demo: 'https://www.electronjs.org/apps/simple-finder',
+        },
+      },
+    ],
+  },
+};
